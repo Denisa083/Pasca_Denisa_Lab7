46e2636358093911964df621e0681d9fb87183db
Denisa Pasca <denisa.pasca083@gmail.com>
2022-11-23 20:59:00 +0200

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Pasca_Denisa_Lab7;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
