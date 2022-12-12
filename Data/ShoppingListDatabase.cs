f1d1fc1eb5b081bd6a9611cdece7990458f97ebc
Denisa Pasca <denisa.pasca083@gmail.com>
2022-12-12 22:20:04 +0200

    final

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -37,6 +37,33 @@ namespace Pasca_Denisa_Lab7.Data
         {
             return _database.Table<Product>().ToListAsync();
         }
+    
+
+public Task<List<ShopList>> GetShopListsAsync()
+        {
+            return _database.Table<ShopList>().ToListAsync();
+        }
+        public Task<ShopList> GetShopListAsync(int id)
+        {
+            return _database.Table<ShopList>()
+            .Where(i => i.ID == id)
+           .FirstOrDefaultAsync();
+        }
+        public Task<int> SaveShopListAsync(ShopList slist)
+        {
+            if (slist.ID != 0)
+            {
+                return _database.UpdateAsync(slist);
+            }
+            else
+            {
+                return _database.InsertAsync(slist);
+            }
+        }
+        public Task<int> DeleteShopListAsync(ShopList slist)
+        {
+            return _database.DeleteAsync(slist);
+        }
         public Task<int> SaveListProductAsync(ListProduct listp)
         {
             if (listp.ID != 0)
